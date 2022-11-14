b87614c497f8db038e806ce14e179160c0d82547
milashkinaa <74614689+milashkinaa@users.noreply.github.com>
2022-11-14 06:03:07 +0300

    updated files

--- assembly/console.s
+++ assembly/console.s
@@ -1,79 +1,149 @@
-	.intel_syntax noprefix                 # используем синтаксис интел
+	.file	"console.c"
+	.intel_syntax noprefix
 	.text
+	.globl	mygetch
+	.type	mygetch, @function
+mygetch:                          # int mygetch()
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	add	rsp, -128
+	lea	rax, -64[rbp]             # tcgetattr( STDIN_FILENO, &oldt );
+	mov	rsi, rax
+	mov	edi, 0
+	call	tcgetattr@PLT         # tcgetattr();
+	mov	rax, QWORD PTR -64[rbp]   # newt = oldt;
+	mov	rdx, QWORD PTR -56[rbp]
+	mov	QWORD PTR -128[rbp], rax
+	mov	QWORD PTR -120[rbp], rdx
+	mov	rax, QWORD PTR -48[rbp]
+	mov	rdx, QWORD PTR -40[rbp]
+	mov	QWORD PTR -112[rbp], rax
+	mov	QWORD PTR -104[rbp], rdx
+	mov	rax, QWORD PTR -32[rbp]
+	mov	rdx, QWORD PTR -24[rbp]
+	mov	QWORD PTR -96[rbp], rax
+	mov	QWORD PTR -88[rbp], rdx
+	mov	rax, QWORD PTR -16[rbp]
+	mov	QWORD PTR -80[rbp], rax
+	mov	eax, DWORD PTR -8[rbp]
+	mov	DWORD PTR -72[rbp], eax
+	mov	eax, DWORD PTR -116[rbp]  # newt.c_lflag &= ~( ICANON | ECHO );
+	and	eax, -11
+	mov	DWORD PTR -116[rbp], eax
+	lea	rax, -128[rbp]            # tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+	mov	rdx, rax
+	mov	esi, 0
+	mov	edi, 0
+	call	tcsetattr@PLT         # tscetattr()
+	call	getchar@PLT           # ch = getchar();
+	mov	DWORD PTR -4[rbp], eax
+	lea	rax, -64[rbp]
+	mov	rdx, rax                  # tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+	mov	esi, 0
+	mov	edi, 0
+	call	tcsetattr@PLT         # tcsetattr()
+	mov	eax, DWORD PTR -4[rbp]
+	leave
+	ret                           # return ch;
+	.size	mygetch, .-mygetch
+	.globl	get_string
+	.type	get_string, @function
+get_string:                       # char* get_string(char* str)
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 32
+	mov	QWORD PTR -24[rbp], rdi
+	mov	DWORD PTR -4[rbp], 0      # int i = 0;
+.L4:
+	mov	eax, 0                    # ch = mygetch();
+	call	mygetch               # mygetch()
+	mov	DWORD PTR -8[rbp], eax
+	mov	rdx, QWORD PTR stdout[rip] # putc(ch, stdout); 
+	mov	eax, DWORD PTR -8[rbp]
+	mov	rsi, rdx
+	mov	edi, eax
+	call	putc@PLT               # putc()
+	mov	eax, DWORD PTR -4[rbp]     # str[i++] = ch;
+	lea	edx, 1[rax]
+	mov	DWORD PTR -4[rbp], edx
+	movsx	rdx, eax
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	edx, DWORD PTR -8[rbp]
+	mov	BYTE PTR [rax], dl        # ch != 4
+	cmp	DWORD PTR -8[rbp], 4      # return &str[i+1];
+	jne	.L4                       # .L4
+	mov	eax, DWORD PTR -4[rbp]    # str[i] = '\0';
+	movsx	rdx, eax
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	BYTE PTR [rax], 0
+	mov	eax, DWORD PTR -4[rbp]    # return &str[i+1];
+	cdqe
+	lea	rdx, 1[rax]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	leave 
+	ret                           # return
+	.size	get_string, .-get_string
 	.section	.rodata
 .LC0:
-	.string	"w"                            # открываем файл на запись  
-.LC1: 
-	.string	"input1.txt"                   # используем файлик input1.txt
+	.string	"w"                   # открываем файлы на запись
+.LC1:
+	.string	"input1.txt"          # input1.txt
 .LC2:
-	.string	"input2.txt"                   # используем файлик input2.txt
+	.string	"input2.txt"          # input2.txt
 	.text
 	.globl	console
 	.type	console, @function
-console:                                   # console()
+console:                          # console()
 	endbr64
 	push	rbp
 	mov	rbp, rsp
 	lea	r11, -16384[rsp]
-.LPSRL0:
+.LPSRL0:                          
 	sub	rsp, 4096
 	or	DWORD PTR [rsp], 0
 	cmp	rsp, r11
 	jne	.LPSRL0
-	sub	rsp, 3648
-	lea	rax, .LC0[rip]              # fopen("input1.txt", "w");
+	sub	rsp, 3632
+	lea	rax, -10016[rbp]    # get_string(str1);
+	mov	rdi, rax
+	call	get_string      # get_string()
+	lea	rax, -20016[rbp]    # get_string(str2);
+	mov	rdi, rax
+	call	get_string      # get_string()
+	lea	rax, .LC0[rip]      # file1 = fopen("input1.txt", "w");
 	mov	rsi, rax
 	lea	rax, .LC1[rip]
 	mov	rdi, rax
-	call	fopen@PLT               # fopen()
-	mov	QWORD PTR -16[rbp], rax
-	lea	rax, .LC0[rip]              # fopen("input2.txt", "w");
+	call	fopen@PLT       # fopen()
+	mov	QWORD PTR -8[rbp], rax
+	lea	rax, .LC0[rip]      # file2 = fopen("input2.txt", "w");
 	mov	rsi, rax
 	lea	rax, .LC2[rip]
 	mov	rdi, rax
-	call	fopen@PLT               # fopen()
-	mov	QWORD PTR -24[rbp], rax
-	mov	DWORD PTR -4[rbp], 0        # int n1 = 0;
-	mov	DWORD PTR -8[rbp], 0        # int n2 = 0;
-	jmp	.L2                         # идем в .L2
-.L3:
-	mov	eax, DWORD PTR -4[rbp]      # ch1[n1] = c;
-	cdqe
-	movzx	edx, BYTE PTR -25[rbp]
-	mov	BYTE PTR -10032[rbp+rax], dl
-	add	DWORD PTR -4[rbp], 1        # n1 += 1;
-.L2:
-	mov	rax, QWORD PTR stdin[rip]   # while ((c = fgetc(stdin)) != EOF)
-	mov	rdi, rax
-	call	fgetc@PLT
-	mov	BYTE PTR -25[rbp], al
-	cmp	BYTE PTR -25[rbp], -1
-	jne	.L3                         # условие не выполняется - идем в .L3
-	jmp	.L4                         # условие выполняется - идем в .L4
-.L5:
-	mov	eax, DWORD PTR -8[rbp]      # ch2[n2] = c;
-	cdqe
-	movzx	edx, BYTE PTR -25[rbp]
-	mov	BYTE PTR -20032[rbp+rax], dl
-	add	DWORD PTR -8[rbp], 1        # n2 += 1;
-.L4:
-	mov	rax, QWORD PTR stdin[rip]   # while ((c = fgetc(stdin)) != EOF)
-	mov	rdi, rax
-	call	fgetc@PLT
-	mov	BYTE PTR -25[rbp], al
-	cmp	BYTE PTR -25[rbp], -1
-	jne	.L5                         # условие выполняется - идем в .L5  
-	mov	rdx, QWORD PTR -16[rbp]     # fprintf(file1, "%s", ch1);
-	lea	rax, -10032[rbp]
+	call	fopen@PLT       # fopen()
+	mov	QWORD PTR -16[rbp], rax
+	mov	rdx, QWORD PTR -8[rbp]
+	lea	rax, -10016[rbp]
 	mov	rsi, rdx
 	mov	rdi, rax
-	call	fputs@PLT               # fprintf()
-	mov	rdx, QWORD PTR -24[rbp]     # fprintf(file2, "%s", ch2);
-	lea	rax, -20032[rbp]
+	call	fputs@PLT        # fputs()
+	mov	rdx, QWORD PTR -16[rbp]
+	lea	rax, -20016[rbp]
 	mov	rsi, rdx
 	mov	rdi, rax
-	call	fputs@PLT               # fprintf()
+	call	fputs@PLT        # fputs()
+	mov	rax, QWORD PTR -8[rbp] # fclose(file1);
+	mov	rdi, rax
+	call	fclose@PLT         # fclose()
+	mov	rax, QWORD PTR -16[rbp] # fclose(file2);
+	mov	rdi, rax
+	call	fclose@PLT         # fclose()
 	nop
 	leave
-	ret                             # return
+	ret                        # return
 	.size	console, .-console
--- assembly/main.s
+++ assembly/main.s
@@ -1,181 +1,175 @@
-	.intel_syntax noprefix        # используем синтаксис интел
-	.text
-	.globl	tv1
-	.bss
-	.align 16
-	.type	tv1, @object
-	.size	tv1, 16
+.intel_syntax noprefix        # используем синтаксис интел
+  .text
+  .globl  tv1
+  .bss
+  .align 16
+  .type  tv1, @object
+  .size  tv1, 16
 tv1:
-	.zero	16
-	.globl	tv2
-	.align 16
-	.type	tv2, @object
-	.size	tv2, 16
+  .zero  16
+  .globl  tv2
+  .align 16
+  .type  tv2, @object
+  .size  tv2, 16
 tv2:
-	.zero	16
-	.globl	dtv
-	.align 16
-	.type	dtv, @object
-	.size	dtv, 16
+  .zero  16
+  .globl  dtv
+  .align 16
+  .type  dtv, @object
+  .size  dtv, 16
 dtv:
-	.zero	16
-	.globl	tz
-	.align 8
-	.type	tz, @object
-	.size	tz, 8
+  .zero  16
+  .globl  tz
+  .align 8
+  .type  tz, @object
+  .size  tz, 8
 tz:
-	.zero	8
-	.text
-	.globl	time_start
-	.type	time_start, @function
+  .zero  8
+  .text
+  .globl  time_start
+  .type  time_start, @function
 time_start:                        # void time_start() { gettimeofday(&tv1, &tz); }
-	endbr64
-	push	rbp
-	mov	rbp, rsp
-	lea	rax, tz[rip]
-	mov	rsi, rax
-	lea	rax, tv1[rip]
-	mov	rdi, rax
-	call	gettimeofday@PLT       # gettimeofday();
-	nop
-	pop	rbp
-	ret
-	.size	time_start, .-time_start
-	.globl	time_stop
-	.type	time_stop, @function
+  endbr64
+  push  rbp
+  mov  rbp, rsp
+  lea  rax, tz[rip]
+  mov  rsi, rax
+  lea  rax, tv1[rip]
+  mov  rdi, rax
+  call  gettimeofday@PLT       # gettimeofday();
+  nop
+  pop  rbp
+  ret
+  .size  time_start, .-time_start
+  .globl  time_stop
+  .type  time_stop, @function
 time_stop:                        # { gettimeofday(&tv2, &tz);
-	endbr64
-	push	rbp
-	mov	rbp, rsp
-	lea	rax, tz[rip]
-	mov	rsi, rax
-	lea	rax, tv2[rip]
-	mov	rdi, rax
-	call	gettimeofday@PLT      # gettimeofday();
-	mov	rax, QWORD PTR tv2[rip]   # dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
-	mov	rdx, QWORD PTR tv1[rip]
-	sub	rax, rdx
-	mov	QWORD PTR dtv[rip], rax
-	mov	rax, QWORD PTR tv2[rip+8] # dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
-	mov	rdx, QWORD PTR tv1[rip+8] 
-	sub	rax, rdx
-	mov	QWORD PTR dtv[rip+8], rax # if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
-	mov	rax, QWORD PTR dtv[rip+8]
-	test	rax, rax
-	jns	.L3                       # .L3
-	mov	rax, QWORD PTR dtv[rip]
-	sub	rax, 1
-	mov	QWORD PTR dtv[rip], rax
-	mov	rax, QWORD PTR dtv[rip+8]
-	add	rax, 1000000
-	mov	QWORD PTR dtv[rip+8], rax
+  endbr64
+  push  rbp
+  mov  rbp, rsp
+  lea  rax, tz[rip]
+  mov  rsi, rax
+  lea  rax, tv2[rip]
+  mov  rdi, rax
+  call  gettimeofday@PLT      # gettimeofday();
+  mov  rax, QWORD PTR tv2[rip]   # dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
+  mov  rdx, QWORD PTR tv1[rip]
+  sub  rax, rdx
+  mov  QWORD PTR dtv[rip], rax
+  mov  rax, QWORD PTR tv2[rip+8] # dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
+  mov  rdx, QWORD PTR tv1[rip+8] 
+  sub  rax, rdx
+  mov  QWORD PTR dtv[rip+8], rax # if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
+  mov  rax, QWORD PTR dtv[rip+8]
+  test  rax, rax
+  jns  .L3                       # .L3
+  mov  rax, QWORD PTR dtv[rip]
+  sub  rax, 1
+  mov  QWORD PTR dtv[rip], rax
+  mov  rax, QWORD PTR dtv[rip+8]
+  add  rax, 1000000
+  mov  QWORD PTR dtv[rip+8], rax
 .L3:
-	mov	rax, QWORD PTR dtv[rip]   # return dtv.tv_sec*1000+dtv.tv_usec/1000;
-	imul	rsi, rax, 1000
-	mov	rcx, QWORD PTR dtv[rip+8]
-	movabs	rdx, 2361183241434822607
-	mov	rax, rcx
-	imul	rdx
-	mov	rax, rdx
-	sar	rax, 7
-	sar	rcx, 63
-	mov	rdx, rcx
-	sub	rax, rdx
-	add	rax, rsi
-	pop	rbp
-	ret
-	.size	time_stop, .-time_stop
-	.section	.rodata
+  mov  rax, QWORD PTR dtv[rip]   # return dtv.tv_sec*1000+dtv.tv_usec/1000;
+  imul  rsi, rax, 1000
+  mov  rcx, QWORD PTR dtv[rip+8]
+  movabs  rdx, 2361183241434822607
+  mov  rax, rcx
+  imul  rdx
+  mov  rax, rdx
+  sar  rax, 7
+  sar  rcx, 63
+  mov  rdx, rcx
+  sub  rax, rdx
+  add  rax, rsi
+  pop  rbp
+  ret
+  .size  time_stop, .-time_stop
+  .section  .rodata
 .LC0:
-	.string	"r"
+  .string  "r"
 .LC1:
-	.string	"input1.txt"
+  .string  "input1.txt"
 .LC2:
-	.string	"input2.txt"
+  .string  "input2.txt"
 .LC3:
-	.string	"%d"
+  .string  "%d"
 .LC4:
-	.string	"Time: %ld \n"
-	.text
-	.globl	main
-	.type	main, @function
+  .string  "Time: %ld \n"
+  .text
+  .globl  main
+  .type  main, @function
 main:                           # int main(int argc, char** argv) 
-	endbr64
-	push	rbp
-	mov	rbp, rsp
-	lea	r11, -16384[rsp]
+  endbr64
+  push  rbp
+  mov  rbp, rsp
+  lea  r11, -16384[rsp]
 .LPSRL0:
-	sub	rsp, 4096
-	or	DWORD PTR [rsp], 0
-	cmp	rsp, r11
-	jne	.LPSRL0
-	sub	rsp, 3664
-	mov	DWORD PTR -20036[rbp], edi
-	mov	QWORD PTR -20048[rbp], rsi
-	lea	rax, .LC0[rip]         # file1 = fopen("input1.txt", "r");
-	mov	rsi, rax
-	lea	rax, .LC1[rip]
-	mov	rdi, rax
-	call	fopen@PLT          # fopen()
-	mov	QWORD PTR -8[rbp], rax # file1 = fopen("input2.txt", "r");
-	lea	rax, .LC0[rip]
-	mov	rsi, rax
-	lea	rax, .LC2[rip]
-	mov	rdi, rax
-	call	fopen@PLT          # fopen()
-	mov	QWORD PTR -16[rbp], rax
-	mov	eax, 0
-	call	time_start         # time_start();
-	lea	rax, -24[rbp]          # scanf("%d", arg);
-	mov	rsi, rax
-	lea	rax, .LC3[rip]
-	mov	rdi, rax
-	mov	eax, 0
-	call	__isoc99_scanf@PLT # scanf()
-	mov	rax, QWORD PTR -24[rbp]# if (arg[0] == 1)
-	cmp	rax, 1
-	jne	.L6                    # .L6
-	mov	eax, 0
-	call	writing@PLT        # writing()
-	jmp	.L7                    # .L7
+  sub  rsp, 4096
+  or  DWORD PTR [rsp], 0
+  cmp  rsp, r11
+  jne  .LPSRL0
+  sub  rsp, 3664
+  mov  DWORD PTR -20036[rbp], edi
+  mov  QWORD PTR -20048[rbp], rsi
+  lea  rax, .LC0[rip]         # file1 = fopen("input1.txt", "r");
+  mov  rsi, rax
+  lea  rax, .LC1[rip]
+  mov  rdi, rax
+  call  fopen@PLT          # fopen()
+  mov  QWORD PTR -8[rbp], rax # file1 = fopen("input2.txt", "r");
+  lea  rax, .LC0[rip]
+  mov  rsi, rax
+  lea  rax, .LC2[rip]
+  mov  rdi, rax
+  call  fopen@PLT          # fopen()
+  mov  QWORD PTR -16[rbp], rax
+  mov  eax, 0
+  call  time_start         # time_start();
+  lea  rax, -24[rbp]          # scanf("%d", arg);
+  mov  rsi, rax
+  lea  rax, .LC3[rip]
+  mov  rdi, rax
+  mov  eax, 0
+  call  __isoc99_scanf@PLT # scanf()
+  mov  rax, QWORD PTR -24[rbp]# if (arg[0] == 1)
+  cmp  rax, 1
+  jne  .L6                    # .L6
+  mov  eax, 0
+  call  writing@PLT        # writing()
+  jmp  .L7                    # .L7
 .L6:
-	mov	rax, QWORD PTR -24[rbp]
-	cmp	rax, 2
-	jne	.L8                    # .L8
-	mov	eax, 0
-	call	files@PLT          # files()
-	jmp	.L7                    # .L7
+  mov  rax, QWORD PTR -24[rbp]
+  cmp  rax, 2
+  jne  .L8                    # .L8
+  mov  eax, 0
+  call  files@PLT          # files()
+  jmp  .L7                    # .L7
 .L8:
-	mov	rax, QWORD PTR -24[rbp]
-	cmp	rax, 3
-	jne	.L9                    # .L9
-	mov	eax, 0
-	call	console@PLT        # console()
-	jmp	.L7                    # .L7
+  mov  rax, QWORD PTR -24[rbp]
+  cmp  rax, 3
+  jne  .L9                    # .L9
+  mov  eax, 0
+  call  console@PLT        # console()
+  jmp  .L7                    # .L7
 .L9:
-	mov	eax, 0                 # 0, который мы потом возвращаем
-	jmp	.L11                   # .L11
-.L7:
-	mov	rdx, QWORD PTR -8[rbp] # fgets(str1, 10000, file1);
-	lea	rax, -10032[rbp]
-	mov	esi, 10000
+  mov  eax, 0                 # 0, который мы потом возвращаем
+  jmp  .L11                   # .L11
+  .L7:
+    mov  rdx, QWORD PTR -8[rbp] # fgets(str1, 10000, file1);
+    lea  rax, -10032[rbp]
+    mov  esi, 10000
+    mov  rdi, rax
+    call  fgets@PLT             # fgets()
+    mov  rdx, QWORD PTR -16[rbp]# fgets(str2, 10000, file2);
+    lea  rax, -20032[rbp]
+    mov  esi, 10000
+    mov  rdi, rax
+    call  fgets@PLT             # fgets()
+	lea	rax, -10032[rbp]        # sort(str1);
 	mov	rdi, rax
-	call	fgets@PLT          # fgets()
-	mov	rdx, QWORD PTR -16[rbp]# fgets(str2, 10000, file2);
-	lea	rax, -20032[rbp]
-	mov	esi, 10000
-	mov	rdi, rax
-	call	fgets@PLT          # fgets()
-	mov	rax, QWORD PTR -8[rbp] #fclose(file1);
-	mov	rdi, rax
-	call	fclose@PLT         # fclose()
-	mov	rax, QWORD PTR -16[rbp] #fclose(file1);
-	mov	rdi, rax
-	call	fclose@PLT         # fclose()
-	lea	rax, -10032[rbp]       # sort(str1);
-	mov	rdi, rax
-	call	sort@PLT           # fsort()
-	lea	rax, -10032[rbp]       # delete_repeats(str1);
+	call	sort@PLT            # sort()
+	lea	rax, -10032[rbp]        # delete_repeats(str1);
 	mov	rdi, rax
 	call	delete_repeats@PLT # delete_repeats()
 	lea	rax, -20032[rbp]       # sort(str2);
@@ -188,16 +182,16 @@ main:                           # int main(int argc, char** argv)
 	lea	rax, -10032[rbp]
 	mov	rsi, rdx
 	mov	rdi, rax
-	call	common_elements@PLT# common_elements()
-	mov	eax, 0                 # printf("Time: %ld \n", time_stop());
-	call	time_stop          # time_stop()
+	call	common_elements@PLT # common_elements();
+	mov	eax, 0                  # common_elements(str1, str2);
+	call	time_stop           # time_stop()
 	mov	rsi, rax
 	lea	rax, .LC4[rip]
 	mov	rdi, rax
 	mov	eax, 0
-	call	printf@PLT         # printf()
+	call	printf@PLT           # printf()
 	mov	eax, 0
 .L11:
 	leave
-	ret                        # return
+	ret
 	.size	main, .-main
